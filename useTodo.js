3a8aa5164e71c2ef074535fb19f57e16462abbcc
luisrodri45 <luisedurodriguez2014@gmail.com>
2022-12-06 19:12:59 -0300

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,54 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+    
+    const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+    const todosCount = todos.length;
+
+    const pendingTodosCount = todos.filter(todo=>!todo.done).length;
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add todo',
+            payload: todo
+        }
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove todo',
+            payload: id
+        });
+    }
+
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: '[TODO] Toggle todo',
+            payload: id
+        })
+
+    }
+
+
+    return{
+        todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todosCount,
+        pendingTodosCount
+    }
+}
